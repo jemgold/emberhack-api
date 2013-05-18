9e4e91f55c6bfce3e613ce67904e1d593b4f3a3c
Jon Gold <hello@designedbygold.com>
2013-05-18 13:32:11 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Eventsapi::Application.config.secret_key_base = 'b5417535fd081dcd317ce0f047f742bb5787827cb919e399467dff0e347078d8ac6d269eb8bf7f25e289b767d79af8fc508265509c5440d68c900c53789b2248'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Eventsapi::Application.config.session_store :cookie_store, key: '_eventsapi_session'
